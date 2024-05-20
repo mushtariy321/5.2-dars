69d20a71f8ad5e1bd791d6fd21c534a1b7a8b4f1
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-05-20 16:33:52 +0300

    done

---
+++ main.js
@@ -0,0 +1,37 @@
+// let arr = [
+//     {
+//         userName: 'Dilbek',
+//         surName: 'Shermatov',
+//         phoneNumber: '900090909',
+//         isMarried: false,
+//         age: '16',
+//         email: 'dilbekpamidor@gmail.com',
+//         address: 'Toshkent viloyati Toshkent Tumani madaniyat',
+//         job: 'student'
+//     }, {
+//         userName: 'Behruz',
+//         surName: 'Rahimjonov',
+//         phoneNumber: '900090909',
+//         isMarried: false,
+//         age: '16',
+//         email: 'dilbekpamidor@gmail.com',
+//         address: 'Toshkent viloyati Toshkent Tumani madaniyat',
+//         job: 'student'
+//     }, {
+//         userName: 'Ulugbak',
+//         surName: 'Ismoilov',
+//         phoneNumber: '900090909',
+//         isMarried: false,
+//         age: '16',
+//         email: 'dilbekpamidor@gmail.com',
+//         address: 'Toshkent viloyati Toshkent Tumani madaniyat',
+//         job: 'student'
+//     }
+// ]
+// let ism = prompt(`Ism kiriting`)
+// let surname = prompt(`Familya kiriting`)
+
+let userName = prompt(`UserName kiriting!`)
+let userID = prompt(`ID kiriting!`)
+let Userpassword = prompt(`Password kiriting!`)
+console.log(`UserName : ${userName}\n UserID : ${userID} \n UserPassword : ${Userpassword}`);
